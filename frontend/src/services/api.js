8ec3ffdbb9c252ce70186233bbb5eed9a567db2f
Thiyumi2003 <thiyumiupasari2003@gmail.com>
2026-07-09 13:29:01 +0530

    update frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,16 @@
 import axios from 'axios';
 
+const getApiBaseUrl = () => {
+  const envUrl = import.meta.env.VITE_API_URL;
+  if (envUrl) {
+    return envUrl.replace(/\/$/, '');
+  }
+
+  return '/api';
+};
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: getApiBaseUrl(),
   headers: {
     'Content-Type': 'application/json'
   },
